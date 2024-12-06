c7534f3c43bff960468b22d9c1928cdcd6c73953
Vusal Hasanov <vhasanov@c3r2s3.42wolfsburg.de>
2024-12-06 17:11:30 +0100

    libft

---
+++ libft.h
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vhasanov <vhasanov@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/15 20:02:00 by vhasanov          #+#    #+#             */
+/*   Updated: 2024/12/06 15:14:22 by vhasanov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+size_t	ft_strlen(const char *str);
+void	*ft_memset(void *ptr, int value, size_t n);
+void	ft_bzero(void *str, size_t n);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
+size_t	ft_strlcpy(char *dest, const char *src, size_t dest_size);
+size_t	ft_strlcat(char *dest, const char *src, size_t dest_size);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+char	*ft_strchr(const char *str, int c);
+char	*ft_strrchr(const char *str, int c);
+int		ft_strncmp(const char *str1, const char *str2, size_t n);
+void	*ft_memchr(const void *ptr, int value, size_t num);
+int		ft_memcmp(const void *ptr1, const void *ptr2, size_t num);
+char	*ft_strnstr(const char *str1, const char *str2, size_t len);
+int		ft_atoi(const char *str);
+void	*ft_calloc(size_t n, size_t element_size);
+char	*ft_strdup(const char *str);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+#endif
